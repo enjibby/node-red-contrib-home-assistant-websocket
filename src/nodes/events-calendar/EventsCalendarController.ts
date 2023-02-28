8f5c009242d1931cf560180064376cabc782df0c
Nathan Brittain <nathan@precedence.com.au>
2023-02-28 21:28:02 +0000

    WIP add js scripts to ts

--- src/nodes/events-calendar/EventsCalendarController.ts
+++ src/nodes/events-calendar/EventsCalendarController.ts
@@ -17,11 +17,43 @@ export default class EventsCalendarController extends OutputController<EventsCal
     #homeAssistant: HomeAssistant;
     #transformState: TransformState;
     #comparatorService: ComparatorService;
+    #schedulerTimeout: NodeJS.Timeout | null;
 
     constructor(params: EventsCalendarControllerConstructor) {
         super(params);
         this.#homeAssistant = params.homeAssistant;
         this.#transformState = params.transformState;
         this.#comparatorService = params.comparatorService;
+        this.#schedulerTimeout = null;
+    }
+
+    onStartScheduler() {
+        if (this.#schedulerTimeout) {
+            return;
+        }
+
+        this.#schedulerTimeout = setTimeout(this.#runSchedule.bind(this), 0);
+    }
+
+    #runSchedule() {
+        const now = new Date();
+
+        const msInterval = this.#getIntervalMs();
+    }
+
+    #getIntervalMs() {
+        let interval: string = this.node.config.updateInterval || '0';
+        if (this.node.config.updateIntervalType === 'expr') {
+            try {
+                interval = this.evaluateJSONata(interval);
+            } catch (e) {
+                this.node.error(
+                    this.RED._('events-calendar.errors.jsonata_error', {
+                        message: e.message,
+                    })
+                );
+                throw new Error('error');
+            }
+        }
     }
 }
